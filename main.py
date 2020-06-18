300f56a693de4f4f638502038050c576e2fe9c07
FeiXue Snow Snow <yyfxsnow@gmail.com>
2020-06-18 17:28:24 +0000

    ADDED EVERYTHING

--- main.py
+++ main.py
@@ -1,5 +1,65 @@
-#GroupWork!
+ORIGINAL = ['M', 'A', 'R', 'S', 'T', 'V']
 
-print("Hello, World!")
-num = input("pls input a number")
-print(num)
+# print the original list
+print('Original list is', ORIGINAL)
+
+# print an emtpy line
+print('')
+
+# make a copy of the original list
+team = ORIGINAL[:]
+
+# 1. change individual element: change the last element to 'X'
+  # add your answer at the beginning of this line
+print(f'1:\t\t{team}')
+
+# 2. add a new element 'Z' at the end of list
+  # add your answer at the beginning of this line
+print(f'2:\t\t{team}')
+
+# 3. add a new element 'B' at the 2nd position (index 1)
+  # add your answer at the beginning of this line
+print(f'3:\t\t{team}')
+
+# 4. remove the third element (index 2) in the list: two ways
+# 4.1 by index 2
+  # add your answer at the beginning of this line
+print(f'4.1:\t{team}')
+# 4.2 by value 'R'
+  # add your answer at the beginning of this line
+print(f'4.2:\t{team}')
+
+# 5. pop an element from the list
+# 5.1 pop the last element and print it out, in one line of code
+  # add your answer at the beginning of this line
+print(f'5.1:\t{team}')
+# 5.1 pop the second element (index 1) and print it out, in one line of code
+  # add your answer at the beginning of this line
+print(f'5.2:\t{team}')
+
+# 6. get the length of the list
+print(f'6:\t\t{team} has', 'length', 'items')  # replace 'length' by a function call for length
+
+# 7. sort the list
+# 7.1 sort the list alphabetically
+  # add your answer at the beginning of this line
+print(f'7.1:\t{team}')
+# 7.2 sort the list in reverse alphabetical order
+  # add your answer at the beginning of this line
+print(f'7.2:\t{team}')
+
+# reset list
+team = ORIGINAL[:]
+
+# 8. reverse the order of the list
+  # add your answer at the beginning of this line
+print(f'8:\t\t{team}')
+
+# 9. print all items in the list, one item in each line
+print('9:')
+# add your answer in next two lines
+
+
+
+# end
+print('\nOur group is fantastic!')
